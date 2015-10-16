139a6aaffc64451d80e3acf16b16b9ee24a70bbe
bm0846 <bm0846@sala7pc13>
2015-10-16 20:52:21 +0200

    Initial Ligia Cano

---
+++ src/main/java/web/presentation/frontController/Dispatcher.java
@@ -0,0 +1,14 @@
+package web.presentation.frontController;
+
+import web.http.HttpRequest;
+import web.http.HttpResponse;
+public class Dispatcher {
+	 public void doGet(HttpRequest request, HttpResponse response) {
+		 
+	 }
+	 
+	 public void doPost(HttpRequest request, HttpResponse response) {
+		 
+	 }
+
+}
